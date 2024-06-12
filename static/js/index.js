b716dd36e56621e84816f08e97b7683573ddbd51
Yingrjimsch <nobel98@bluewin.ch>
2024-06-13 01:21:04 +0200

    chore: make bibitex copiable

--- static/js/index.js
+++ static/js/index.js
@@ -19,3 +19,25 @@ $(document).ready(function() {
     bulmaSlider.attach();
 
 })
+
+function copyCode() {
+    const codeBlock = document.getElementById('codeBlock').innerText;
+	const copyButton = document.getElementById('copyButton');
+    navigator.clipboard.writeText(codeBlock).then(() => {
+		copyButton.innerHTML = `
+            <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="16" height="16" viewBox="0 0 72 72">
+<path d="M57.658,12.643c1.854,1.201,2.384,3.678,1.183,5.532l-25.915,40c-0.682,1.051-1.815,1.723-3.064,1.814 C29.764,59.997,29.665,60,29.568,60c-1.146,0-2.241-0.491-3.003-1.358L13.514,43.807c-1.459-1.659-1.298-4.186,0.36-5.646 c1.662-1.46,4.188-1.296,5.646,0.361l9.563,10.87l23.043-35.567C53.329,11.971,55.806,11.442,57.658,12.643z"></path>
+</svg>
+            Copied
+        `;
+        setTimeout(() => {
+            copyButton.innerHTML = `
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="none" viewBox="0 0 24 24" class="icon-sm"><path fill="currentColor" fill-rule="evenodd" d="M7 5a3 3 0 0 1 3-3h9a3 3 0 0 1 3 3v9a3 3 0 0 1-3 3h-2v2a3 3 0 0 1-3 3H5a3 3 0 0 1-3-3v-9a3 3 0 0 1 3-3h2zm2 2h5a3 3 0 0 1 3 3v5h2a1 1 0 0 0 1-1V5a1 1 0 0 0-1-1h-9a1 1 0 0 0-1 1zM5 9a1 1 0 0 0-1 1v9a1 1 0 0 0 1 1h9a1 1 0 0 0 1-1v-9a1 1 0 0 0-1-1z" clip-rule="evenodd"></path></svg>
+            	Copy
+            `;
+        }, 2000); // Reset button after 2 seconds
+
+    }).catch(err => {
+        console.error('Failed to copy: ', err);
+    });
+}
